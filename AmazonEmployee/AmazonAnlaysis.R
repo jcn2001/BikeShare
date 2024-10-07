35f322258b339ea52ccb17c3ef1024d6711e78a4
unknown <nucannon11@gmail.com>
2024-10-07 09:40:04 -0600

    read in the data and created the .R file

---
+++ AmazonEmployee/AmazonAnlaysis.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+library(tidymodels)
+library(vroom)
+
+
+amazon_train_data <- vroom("C:/Users/Josh/Documents/stat348/AmazonEmployee/amazon-employee-access-challenge/train.csv")
+
