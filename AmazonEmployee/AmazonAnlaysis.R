dd5a909a916cfb72fe48c6d34465ac5896a879de
unknown <nucannon11@gmail.com>
2024-10-16 10:05:19 -0600

    fit knn

--- AmazonEmployee/AmazonAnlaysis.R
+++
@@ -1,7 +0,0 @@
-library(tidyverse)
-library(tidymodels)
-library(vroom)
-
-
-amazon_train_data <- vroom("C:/Users/Josh/Documents/stat348/AmazonEmployee/amazon-employee-access-challenge/train.csv")
-
